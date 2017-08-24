9ac12682c7f6d91d7f84364a0f4034413529e61d
Erez Weiss <erez.weiss@jivesoftware.com>
2017-08-24 16:49:42 +0300

    refactor: my order css

--- src/orderSummary.component.jsx
+++ src/orderSummary.component.jsx
@@ -7,11 +7,14 @@ class OrderSummary extends Component {
     return (
       <div className="e-order-summary">
         <div className="e-order-list">
-        {App.state && App.state.orders && App.state.orders.map(ord => {
-          return JSON.stringify(ord);
-        })}
+          <div className="e-order-list-title"><h1>My Order</h1></div>
+
+          {App.state && App.state.orders && App.state.orders.map(ord => {
+            return <p>Pizza {ord.topping} {ord.price}$</p>
+            // return JSON.stringify(ord);
+          })}
         </div>
-        <div className="e-order-total">Total: {App.state.orderTotal || 0}</div>
+        <div className="e-order-total"><b>Order Total: </b>{App.state.orderTotal || 0}$</div>
       </div>
     );
   }
